ce7d7265d757044a05552112ff1c88765ddc8a61
schandala@rodanandfields.com <sarathchandrach84@gmail.com>
2022-12-26 10:40:23 -0800

    Updates

---
+++ limits-webservies/src/main/java/com/demodel/rest/webservices/limitswebservies/LimitsWebserviesApplication.java
@@ -0,0 +1,13 @@
+package com.demodel.rest.webservices.limitswebservies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsWebserviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsWebserviesApplication.class, args);
+	}
+
+}
---
+++ limits-webservies/src/test/java/com/demodel/rest/webservices/limitswebservies/LimitsWebserviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demodel.rest.webservices.limitswebservies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsWebserviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
