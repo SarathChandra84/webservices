39cf79d0ef9ebdfdb6a1ff8192d160655cc86df6
schandala@rodanandfields.com <schandala@rodanandfields.com>
2022-12-30 06:52:08 -0800

    updated changes

---
+++ demo-rest-microservice/src/main/java/com/demo/rest/microservices/demorestmicroservice/DemoRestMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.rest.microservices.demorestmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestMicroserviceApplication.class, args);
+	}
+
+}
---
+++ graphql-demo/src/test/java/com/graphql/rest/webservices/graphqldemo/GraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.rest.webservices.graphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
